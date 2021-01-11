2dac61c9d885c9f1bcc8539768a889fa7bb32e05
Yasin ULUSOY <yasin.ulusoy@yahoo.com>
2021-01-11 04:04:01 +0300

    moved our directory and shop data into its own reducer,created respective selectors and updated coresponding compnonents with new redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 import {combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 
-export default combineReducers({
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']
+    //whitelist localstorage ta hangi objeyi tutmak istediğimizi belirtiyor
+    //Zaten user objesi firebase tarafında tutulduğu için tekrar localde tutmamıza gerek yok
+
+}
+
+const rootReducer=combineReducers({
     user:userReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    directory:directoryReducer,
+    shop:shopReducer
 })
+
+export default persistReducer(persistConfig,rootReducer)
